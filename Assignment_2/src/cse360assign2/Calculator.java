f392d4e6603187d4f052320cb3e42832b710dc2e
Lauren Gold <llgold@asu.edu>
2019-02-19 11:13:50 -0700

    intitial checkin of assignment 2

---
+++ Assignment_2/src/cse360assign2/Calculator.java
@@ -0,0 +1,57 @@
+/**
+ * @author Lauren Gold
+ * @version %I%
+ *  class id: 383
+ *  Assignment2
+ *  Description: 
+ *  
+ *  
+ *  
+ *  
+ *  
+ */
+
+package cse360assign2;
+
+//the <Calculator> class does many different calculations
+public class Calculator {
+
+	private int total;
+	
+	//the Calculator method is the constructor for this class
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	// the getTotal method returns the total for this calculation
+	public int getTotal () {
+		return 0;
+	}
+	
+	// the add method adds integers @param value to be tested
+	public void add (int value) {
+		
+	}
+	
+	// the subtract method subtracts integers @param value to be tested
+	public void subtract (int value) {
+		
+	}
+	
+	//the multiply method multiplies integers @param value to be tested
+	public void multiply (int value) {
+		
+	}
+	
+	//the divide method divides integers @value to be tested
+	public void divide (int value) {
+		
+	}
+	
+	/*
+	 * the getHistory method returns a string @return value to display this calculator's history
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
